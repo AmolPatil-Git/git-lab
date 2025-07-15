a89cfb2e92336a517549efaea5e273aebd91e910
Amol Patil <pamol1781994@gmail.com>
2025-07-15 13:25:39 +0530

    4u

--- file1 copy 4.js
+++ file1 copy 4.js
@@ -1,18 +1,1 @@
-console.log("This is file1.js");
-export const file1 = "This is the content of file1.js";
-export function greet() {
-    console.log("Hello from file1.js");
-}
-export class File1Class {           
-    constructor() {
-        this.name = "File1Class";
-    }
-    sayHello() {
-        console.log(`Hello from ${this.name}`);
-    }
-}
-export default function defaultExport() {
-    console.log("This is the default export from file1.js");
-}
-export { default as defaultExport } from './file1.js';
-export * from './file1.js';     
+console.log("This is file4.js");
